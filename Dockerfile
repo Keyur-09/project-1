df054305c59d867b54906f27d50bacd2c75b4a63
Keyur-09 <keyur.kyada@corenettechltd.com>
2026-01-28 14:55:32 +0530

    deploy app locally using Docker on port 9800

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ FROM node:18-alpine
 WORKDIR /app
 
 COPY package*.json ./
-
 RUN npm install
 
 COPY . .
 
-CMD [ "node", "index.js" ]
+EXPOSE 9800
+
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("App container started and ready to connect to DB");
+const http = require("http");
+
+const PORT = 9800;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Local CI/CD deployment working");
+});
+
+server.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
